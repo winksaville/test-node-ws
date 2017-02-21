031eb408d8036145cb5a6046dfbacd85f493bba4
Wink Saville <wink@saville.com>
2017-02-21 15:59:04 -0800

    Add support for a SPA
    Added using webpack and mithril to create a Single Page App (SPA) with
    a simple nodejs server with a minimum of dependencies.

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 let http = require('http');
+let fs = require('fs')
 
 const PORT = 3000;
 
-let server = http.createServer( (req, res) => {
-  res.end('<body>Hello, click to <a href=http://localhost:' + PORT + req.url + '>reload</a></body>');
+// Create a server and the handler for a few requests
+var server = http.createServer((req, res) => {
+  console.log('req.url=' + req.url);
+  switch (req.url) {
+    case '/': {
+      res.writeHead(200, {
+        'content-type': 'text/html',
+        'charset': 'UTF-8'
+      });
+
+      fs.createReadStream('./index.html').pipe(res)
+      break;
+    }
+    case '/client.js': {
+      res.writeHead(200, {
+        'content-type': 'text/javascript',
+        'charset': 'UTF-8'
+      });
+
+      fs.createReadStream('./client.js').pipe(res)
+      break;
+    }
+    default: {
+      console.log('default: req.url=' + req.url);
+      res.writeHead(404, {
+        'content-type': 'text/html',
+        'charset': 'UTF-8'
+      });
+
+      res.end('bad req.url=' + req.url);
+      break;
+    }
+  }
 });
 
+// Start it listening on the desired port
 server.listen(PORT, () => {
   console.log('Listening on: http://localhost:%s', PORT);
 });
