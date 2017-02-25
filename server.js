0b78a93a8392ce4e1e7d70bfb251c817701b5e68
Wink Saville <wink@saville.com>
2017-02-24 22:41:03 -0800

    Fix issue #3, convert server.js to TypeScript, server.ts.
    The biggest problem was getting the import statements correct, for some
    unknown reason I had to specifically use:
    
      import * as WebSocket from 'ws';
    
    If I used:
    
      import * as ws from 'ws';
    
    I ran into a compile time bug that
    ws_server.on('connection', (client: WebSocket) ...
    was wrong argument type:
    
      $ tsc
      server.ts(79,28): error TS2345: Argument of type '(client: WebSocket) => void' is not assignable to parameter of type '() => void'.
      server.ts(85,10): error TS2339: Property 'on' does not exist on type 'WebSocket'.
      server.ts(91,10): error TS2339: Property 'on' does not exist on type 'WebSocket'.
    
    The compiler seems to not understand that WebSocket was a type and instead
    was interpreting it as a namespace in the statements. I say this because
    there are two definitions for WebSocket in
    node_modules/@types/ws/websocket/index.d.ts.
    
    One as a class:
    
      declare class WebSocket extends events.EventEmitter {
          static CONNECTING: number;
          ...
    
    And the other as a namespace:
    
      declare namespace WebSocket {
          ...
    
    
    Anyway, that is my guess as to the problem, maybe someday I'll find
    the real reason.

--- server.js
+++ server.js
@@ -1,138 +1,110 @@
-let http = require('http');
-let fs = require('fs');
-let ws = require('ws');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const fs = require("fs");
+const WebSocket = require("ws");
 const debug = require('debug')('my-server');
-
 const PORT = 3000;
-
 // Create a server and the handler for a few requests
-let http_server = http.createServer((req, res) => {
-  debug('req.url=' + req.url);
-  switch (req.url) {
-    case '/': {
-      res.writeHead(200, {
-        'content-type': 'text/html',
-        'charset': 'UTF-8'
-      });
-
-      fs.createReadStream('./index.html').pipe(res)
-      break;
+const http_server = http.createServer((req, res) => {
+    debug('req.url=%s res=%s', req.url, res);
+    switch (req.url) {
+        case '/': {
+            res.writeHead(200, {
+                'content-type': 'text/html',
+                'charset': 'UTF-8'
+            });
+            fs.createReadStream('./index.html').pipe(res);
+            break;
+        }
+        case '/bundle.js': {
+            res.writeHead(200, {
+                'content-type': 'text/javascript',
+                'charset': 'UTF-8'
+            });
+            fs.createReadStream('./bundle.js').pipe(res);
+            break;
+        }
+        default: {
+            debug('default: req.url=' + req.url);
+            res.writeHead(404, {
+                'content-type': 'text/html',
+                'charset': 'UTF-8'
+            });
+            res.end('bad req.url=' + req.url);
+            break;
+        }
     }
-    case '/bundle.js': {
-      res.writeHead(200, {
-        'content-type': 'text/javascript',
-        'charset': 'UTF-8'
-      });
-
-      fs.createReadStream('./bundle.js').pipe(res)
-      break;
-    }
-    default: {
-      debug('default: req.url=' + req.url);
-      res.writeHead(404, {
-        'content-type': 'text/html',
-        'charset': 'UTF-8'
-      });
-
-      res.end('bad req.url=' + req.url);
-      break;
-    }
-  }
 });
-
 // Start it listening on the desired port
 http_server.listen(PORT, () => {
-  debug('Listening on: http://localhost:%s', PORT);
+    debug('Listening on: http://localhost:%s', PORT);
 });
-
-
-// Create WebSocket Server and handle some events
-let ws_server = new ws.Server({server: http_server});
-
 function secs2ms(secs) {
-  return secs * 1000;
+    return secs * 1000;
 }
-
 var conn_id = 0;
-
-function Connection() {
-  this.id = conn_id++;
-  this.toString = () => {
-    return `Connection: id=${this.id}`;
-  };
+class Connection {
+    constructor() {
+        this.id = conn_id++;
+    }
+    toString() {
+        return `Connection: id=${this.id} delay=${this.delay} spacing=${this.spacing} timeout=${this.timeout}`;
+    }
 }
-
-ws_server.on('connection', (ws) => {
-
-  let conn = new Connection();
-  conn.ws = ws;
-  debug('onConnect: conn=%s', conn.toString());
-  startEventGenerator(conn, secs2ms(10), secs2ms(3));
-
-  ws.on('close', (code, reason) => {
-    debug('ws: closed code=%d reason=\'%s\' conn=%s',
-      code, reason, conn.toString());
-    stopEventGenerator(conn);
-  });
-
-  ws.on('message', (msg, masked) => {
-    debug('ws_server: message msg=%s masked=%s',
-      msg.data, masked);
-  });
+// Create WebSocket Server and handle some events
+let ws_server = new WebSocket.Server({ server: http_server });
+ws_server.on('connection', (client) => {
+    let conn = new Connection();
+    conn.client = client;
+    debug('onConnect: conn=%s', conn.toString());
+    startEventGenerator(conn, secs2ms(10), secs2ms(3));
+    client.on('close', (code, reason) => {
+        debug('client: closed code=%d reason=\'%s\' conn=%s', code, reason, conn.toString());
+        stopEventGenerator(conn);
+    });
+    client.on('message', (msg, flags) => {
+        debug('client: message msg=%s flags=%s', msg, JSON.stringify(flags));
+    });
 });
-
 ws_server.on('error', (err) => {
-  debug('ws_server: error err=%s', err);
+    debug('ws_server: error err=%s', err);
 });
-
 ws_server.on('headers', (headers) => {
-  debug('ws_server: headers=%s', JSON.stringify(headers));
+    debug('ws_server: headers=%s', JSON.stringify(headers));
 });
-
 ws_server.on('listening', () => {
-  debug('ws_server: listening');
+    debug('ws_server: listening');
 });
-
 function startEventGenerator(conn, delay, spacing) {
-  debug('startEventGenerator:+ delay=%d spacing=%d conn=%s',
-    delay, spacing, conn.toString());
-
-  conn.delay = delay;
-  conn.spacing = spacing;
-  conn.timeout = 0;
-  let prevToString = conn.toString;
-  conn.toString = () => {
-    return prevToString() + ` delay=${conn.delay} spacing=${conn.spacing} timeout=${conn.timeout}`;
-  };
-
-  conn.timeoutObj = setTimeout(() => {
-    timeout(conn, 'initial');
-  }, conn.delay);
-
-  debug('startEventGenerator:- delay=%d spacing=%d conn=%s',
-    delay, spacing, conn.toString());
+    debug('startEventGenerator:+ delay=%d spacing=%d conn=%s', delay, spacing, conn.toString());
+    conn.delay = delay;
+    conn.spacing = spacing;
+    conn.timeout = 0;
+    conn.timeoutObj = setTimeout(() => {
+        timeout(conn, 'initial');
+    }, conn.delay);
+    debug('startEventGenerator:- delay=%d spacing=%d conn=%s', delay, spacing, conn.toString());
 }
-
 function stopEventGenerator(conn) {
-  debug('stopEventGenerator:+ conn=%s', conn.toString());
-
-  if (conn.timeoutObj) {
-    clearTimeout(conn.timeoutObj);
-    conn.timeoutObj = null;
-  }
-
-  debug('stopEventGenerator:- conn=%s', conn.toString());
+    debug('stopEventGenerator:+ conn=%s', conn.toString());
+    if (conn.timeoutObj) {
+        clearTimeout(conn.timeoutObj);
+        conn.timeoutObj = null;
+    }
+    debug('stopEventGenerator:- conn=%s', conn.toString());
 }
-
 function timeout(conn, str) {
-  if (conn.timeoutObj) {
-    conn.ws.send(`hi client ${conn.timeout}`);
-    conn.timeout += 1;
-    debug('timeout: str=%s conn=%s', str, conn.toString());
-    setTimeout(() => {
-      timeout(conn, 'continuing');
-    }, conn.spacing);
-  } else {
-    debug('timeout: stopped');
-  }
+    if (conn.timeoutObj) {
+        conn.client.send(`hi client ${conn.timeout}`);
+        conn.timeout += 1;
+        debug('timeout: str=%s conn=%s', str, conn.toString());
+        setTimeout(() => {
+            timeout(conn, 'continuing');
+        }, conn.spacing);
+    }
+    else {
+        debug('timeout: stopped');
+    }
 }
+//# sourceMappingURL=server.js.map
---
+++ server.ts
@@ -0,0 +1,143 @@
+import * as http from 'http';
+import * as fs from 'fs';
+import * as WebSocket from 'ws';
+
+const debug = require('debug')('my-server');
+
+const PORT: number = 3000;
+
+// Create a server and the handler for a few requests
+const http_server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  debug('req.url=%s res=%s', req.url, res);
+  switch (req.url) {
+    case '/': {
+      res.writeHead(200, {
+        'content-type': 'text/html',
+        'charset': 'UTF-8'
+      });
+
+      fs.createReadStream('./index.html').pipe(res)
+      break;
+    }
+    case '/bundle.js': {
+      res.writeHead(200, {
+        'content-type': 'text/javascript',
+        'charset': 'UTF-8'
+      });
+
+      fs.createReadStream('./bundle.js').pipe(res)
+      break;
+    }
+    default: {
+      debug('default: req.url=' + req.url);
+      res.writeHead(404, {
+        'content-type': 'text/html',
+        'charset': 'UTF-8'
+      });
+
+      res.end('bad req.url=' + req.url);
+      break;
+    }
+  }
+});
+
+// Start it listening on the desired port
+http_server.listen(PORT, () => {
+  debug('Listening on: http://localhost:%s', PORT);
+});
+
+
+function secs2ms(secs: number) {
+  return secs * 1000;
+}
+
+var conn_id = 0;
+
+class Connection {
+  public id: number;
+  public timeout: number;
+  public timeoutObj: any;
+  public delay: number;
+  public spacing: number;
+  public client: WebSocket;
+
+  constructor() {
+    this.id = conn_id++;
+  }
+
+  toString() : string {
+    return `Connection: id=${this.id} delay=${this.delay} spacing=${this.spacing} timeout=${this.timeout}`;
+  }
+}
+
+// Create WebSocket Server and handle some events
+let ws_server = new WebSocket.Server({server: http_server});
+
+ws_server.on('connection', (client: WebSocket) => {
+  let conn = new Connection();
+  conn.client = client;
+  debug('onConnect: conn=%s', conn.toString());
+  startEventGenerator(conn, secs2ms(10), secs2ms(3));
+
+  client.on('close', (code: number, reason: string) => {
+    debug('client: closed code=%d reason=\'%s\' conn=%s',
+      code, reason, conn.toString());
+    stopEventGenerator(conn);
+  });
+
+  client.on('message', (msg: string, flags: {binary: boolean}) => {
+    debug('client: message msg=%s flags=%s',
+      msg, JSON.stringify(flags));
+  });
+});
+
+ws_server.on('error', (err: Error) => {
+  debug('ws_server: error err=%s', err);
+});
+
+ws_server.on('headers', (headers: string[]) => {
+  debug('ws_server: headers=%s', JSON.stringify(headers));
+});
+
+ws_server.on('listening', () => {
+  debug('ws_server: listening');
+});
+
+function startEventGenerator(conn: Connection, delay: number, spacing: number) {
+  debug('startEventGenerator:+ delay=%d spacing=%d conn=%s',
+    delay, spacing, conn.toString());
+
+  conn.delay = delay;
+  conn.spacing = spacing;
+  conn.timeout = 0;
+  conn.timeoutObj = setTimeout(() => {
+    timeout(conn, 'initial');
+  }, conn.delay);
+
+  debug('startEventGenerator:- delay=%d spacing=%d conn=%s',
+    delay, spacing, conn.toString());
+}
+
+function stopEventGenerator(conn: Connection) {
+  debug('stopEventGenerator:+ conn=%s', conn.toString());
+
+  if (conn.timeoutObj) {
+    clearTimeout(conn.timeoutObj);
+    conn.timeoutObj = null;
+  }
+
+  debug('stopEventGenerator:- conn=%s', conn.toString());
+}
+
+function timeout(conn: Connection, str: string) {
+  if (conn.timeoutObj) {
+    conn.client.send(`hi client ${conn.timeout}`);
+    conn.timeout += 1;
+    debug('timeout: str=%s conn=%s', str, conn.toString());
+    setTimeout(() => {
+      timeout(conn, 'continuing');
+    }, conn.spacing);
+  } else {
+    debug('timeout: stopped');
+  }
+}
